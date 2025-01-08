9971c27fa461a10b4990d490d8a28e2e5a51e73e
j12151995 <j12151995@gmail.com>
2025-01-08 21:39:04 +0800

    Update 認識資料類型

--- Assets/scripts/Class_2_DataType.cs
+++ Assets/scripts/Class_2_DataType.cs
@@ -21,4 +21,19 @@ public class Class_2_DataType : MonoBehaviour
     public string character = "發條";
     public bool isDead = false;     // 是否死亡 : 否
     public bool gameOver = true;    // 是否結束 : 是
+
+
+    // 整數資料類型
+    public byte lv = 18;            // byte 型別，範圍 0~255 大小 8 byte
+    public uint coin = 9999;        // uint 型別，範圍 0~4294**，大小 32 byte
+    public long items = 3000;       // long 型別，範圍 0~92***，大小 64 byte
+
+
+    // 字串與字元
+    public string playerName = "Rondo";  //字串 : 儲存多個字元，使用雙引號
+    public char a = 'a';                 //字元 : 儲存單個字元，使用單引號
+
+    // 益位
+    //public sbyte number = 128;  <---超過 sbye的轉換值    // 益位會導致錯誤，會出現紅色蚯蚓
+
 }
