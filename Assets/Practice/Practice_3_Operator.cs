3f06b6ecf9ed5f9bbaa00329c2fdb6bafe35fa76
j12151995 <j12151995@gmail.com>
2025-01-20 02:45:16 +0800

    Update 完成作業

---
+++ Assets/Practice/Practice_3_Operator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+//這是作業
+public class Practice_3_Operator : MonoBehaviour 
+{
+    private void Awake()
+    {
+        int numberA = 2;
+        Debug.Log(6 / numberA*(1 + 2));
+    }
+}
---
+++ Assets/Practice/Practice_4_Function.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+// 這是作業
+public class Practice_4_Function : MonoBehaviour 
+{
+    private void Awake()
+    {
+        bool X = true;
+        bool Y = false;
+        Debug.Log($"<color=#F93>X並且Y{X && Y}</color>");
+        Debug.Log($"<color=#F93>X或者Y{X || Y}</color>");
+
+    }
+
+
+
+
+    private bool Operator(bool X,bool Y)
+    {
+        return X && Y;
+        return X || Y;
+        
+    }
+}
