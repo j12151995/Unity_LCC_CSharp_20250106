8fb7de09e7ebbef3e5094af522841278e00027de
j12151995 <j12151995@gmail.com>
2025-02-10 21:41:09 +0800

    Update 認識屬性一點點靜態

---
+++ Assets/scripts/Class_7_1_GetSetData.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Inferno
+{
+    /// <summary>
+    /// 練習存取資料用
+    /// </summary>
+
+    public class Class_7_1_GetSetData : MonoBehaviour
+
+    {
+        public Class_7_Property property;
+
+        private void Awake()
+        {
+            //取得另一個類別的資料
+            Debug.Log(property.moveSpeed);     //可取得公開的變數 也就是另一個資料的pulic
+            //Debug.Log(property.rotatespeed);   //不可取得私人的變數 也就是另一個資料的 private
+
+
+            //設定另一個類別的資料
+            property.moveSpeed = 7.7f;        //可取得公開的變數
+            // property.rotatespeed = 45.1f;  //不可取得私人的變數
+
+            Debug.Log(property.runSpeed);   //可取得公開的變數
+            //Debug.Log(property.sprintSpeed);   //不可取得私人的變數
+
+            property.runSpeed = 50.3f;        //可以設定有 set的屬性
+            //property.jumpSpeed = 99.3f;        //不可以設定沒有 set的屬性(唯獨)
+
+            property.Lv = 1;
+            
+
+        }
+    }
+}
+
--- Assets/scripts/Class_7_Property.cs
+++ Assets/scripts/Class_7_Property.cs
@@ -11,11 +11,83 @@ namespace Inferno
     public class Class_7_Property : MonoBehaviour
     {
 
+        #region 認識屬性
         //公開的變數 : 可以讓外部讀取與寫入
-        public float movespeed = 3.5f;
+        public float moveSpeed = 3.5f;
 
 
         //私人的變數 : 不允許外部讀取與寫入
-        private float rotatespeed = 12.7f;
+        private float rotateSpeed = 12.7f;
+
+
+        //屬性開始
+        //公開的屬性 : 允許存取
+        public float runSpeed { get; set; }
+        //私人的屬性 : 不允許存取
+        private float sprintSpeed { get; set; }
+
+        //公開的屬性 : 但只有get 代表唯獨(只能讀取不能寫入)
+        public float jumpSpeed { get; }
+
+        //prop+Tab*2 快速完成
+        public byte Lv { get; set; } = 1;  //有預設值的屬性
+
+
+        private void Awake()
+        {
+            Debug.Log("------Damn 取得變數與屬性-----");
+            Debug.Log(moveSpeed);
+            Debug.Log(rotateSpeed);
+            Debug.Log(runSpeed);
+            Debug.Log(sprintSpeed);
+            Debug.Log(Lv);
+            Debug.Log("------Damn 取得變數與屬性-----");
+            moveSpeed = 77.7f;
+            rotateSpeed = 45.1f;                      //私人的變數在同一個類別內可以設定 (在其他類別會被拒絕)
+            runSpeed = 30;
+            sprintSpeed = 70.7f;
+            //jumpspeed = 99;                         // 沒有set 的屬性在類別內也無法設定
+            Lv = 6;
+            
+        }
+        #endregion
+
+
+        //屬性簡寫 
+        public float hp { get; set; } = 100;
+
+        //屬性標準寫法
+        // 用來裝屬性 mp 的容器
+        private float _mp = 50;
+
+        //屬性是給予外部存取權限的窗口
+        public float mp
+        {
+            get 
+            {
+                return _mp;
+            }
+            set
+            {
+                _mp = value;
+            }
+        }
+
+        private void Start()
+        {
+            Debug.Log($"<color=#77f>魔力 : {mp}</color>");
+            mp = 500;
+            Debug.Log($"<color=#77f>魔力 : {mp}</color>");
+        }
+
+        //取得屬性的簡寫(箭頭 => lambda)
+        //唯獨屬性 防禦值是 30
+        public int defense { get; } = 30;
+        //唯獨屬性 攻擊力是50
+        public int attack => 50;
+
+
+
+
     }
 }
